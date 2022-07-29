86b7771720e3e6e1a4b33b57776be243584cc53c
sarthak-kanodia <kanodiasarthak.2k18@gmail.com>
2022-07-29 21:42:55 +0530

    Number Converter Structure

---
+++ src/main/java/com/virtusa/NumberConverter/App.java
@@ -0,0 +1,13 @@
+package com.virtusa.NumberConverter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
